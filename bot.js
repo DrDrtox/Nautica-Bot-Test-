0823132bdba3b58a49bf5b477099c6f8abe9c56c
drtox <45079652+drtox@users.noreply.github.com>
2018-12-31 01:16:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,26 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '-'
+const client = new Discord.Client():
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`Start ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-	
+        console.log('╔[════════════]╗')
+        console.log(' Bot Is Online')
+        console.log('╚[════════════]╝')
+        console.log('')
+        console.log('╔[════════════════════════════════════]╗');
+        console.log('')
+        console.log(' Logged in as * [ " ${client.user.username} " ] ');
+        console.log( ' Welcome Bro , UserName * [ " ${client.user.username} " ]' );
+        console.log(  :  * [ " ${client.user.tag} " ]);
+        console.log('')
+        console.log('╚[════════════════════════════════════]╝')
+        console.log('')
+        console.log('╔[════════════]╗')
+        console.log('')
+        console.log( 'Have Fun - Mal Team  ')
+        console.log('')
+        console.log('╚[════════════]╝')
+        
 });
-
 
 client.on('message', message => {
     var prefix = "-"
